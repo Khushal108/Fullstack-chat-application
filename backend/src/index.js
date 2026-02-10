7f151b68d7aaf3dc4f1359e40d9b3ca8cf9f27b7
Khushal Mishra <khushalmishra001@gmail.com>
2026-02-10 11:53:37 +0530

    Fixed path in producton

--- backend/src/index.js
+++ backend/src/index.js
@@ -26,12 +26,10 @@ app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
 
 if (process.env.NODE_ENV === "production") {
-  const frontendPath = path.join(__dirname, "../frontend/dist");
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.use(express.static(frontendPath));
-
-   app.get("/{*splat}", (req, res) => {
-    res.sendFile(path.join(frontendPath, "index.html"));
+  app.get("/{*splat}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
